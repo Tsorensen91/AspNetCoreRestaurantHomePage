eb535976015b229b0cf1c91d1b547904363ccf70
1721102 <1721102@chester.ac.uk>
2018-10-22 10:55:15 +0100

    Incl. Razor page

---
+++ Restaurant-page/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Restaurant_page.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
